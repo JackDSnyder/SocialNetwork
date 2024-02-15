56ff27ab5860728aece1e3ad9ef285d1db63c514
Jack Snyder <jacksnyde@gmail.com>
2024-02-15 14:28:55 -0700

    Update

---
+++ add.py
@@ -0,0 +1,18 @@
+import random
+names = [
+    'John', 'Emma', 'Liam', 'Olivia', 'Noah', 'Ava', 'William', 'Sophia', 'James', 'Isabella',
+    'Oliver', 'Mia', 'Benjamin', 'Charlotte', 'Elijah', 'Amelia', 'Lucas', 'Harper', 'Mason', 'Evelyn',
+    'Logan', 'Abigail', 'Alexander', 'Emily', 'Ethan', 'Elizabeth', 'Jacob', 'Avery', 'Michael', 'Sofia',
+    'Daniel', 'Ella', 'Henry', 'Madison', 'Jackson', 'Scarlett', 'Sebastian', 'Victoria', 'Jack', 'Aria',
+    'Aiden', 'Grace', 'Matthew', 'Chloe', 'Samuel', 'Camila', 'David', 'Penelope', 'Joseph', 'Riley',
+    'Carter', 'Luna', 'Owen', 'Hannah', 'Wyatt', 'Nora', 'Jayden', 'Lily', 'John', 'Zoey', 'Luke', 'Mila',
+    'Gabriel', 'Layla', 'Anthony', 'Aubrey', 'Isaac', 'Lillian', 'Grayson', 'Addison', 'Levi', 'Eleanor',
+    'Christopher', 'Natalie', 'Joshua', 'Brooklyn', 'Andrew', 'Stella', 'Lincoln', 'Hazel', 'Mateo', 'Ellie',
+    'Ryan', 'Paisley', 'Jaxon', 'Audrey', 'Nathan', 'Skylar', 'Aaron', 'Violet', 'Isaiah', 'Claire',
+    'Charles', 'Bella', 'Hunter', 'Aurora', 'Christian', 'Lucy', 'Thomas', 'Anna'
+]
+
+
+for name in names:
+    num = random.randrange(1,999)
+    print(f"INSERT INTO accounts('{name}@gmail.com','{name}{name}{num}' )")
